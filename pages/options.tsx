676fcf8c59e220cdf0c63e1bcd254b7ec8f858e9
ChocoFloco <149216961+ChocoFloco@users.noreply.github.com>
2023-10-29 13:17:18 -0500

    Update options.tsx

--- pages/options.tsx
+++ pages/options.tsx
@@ -176,7 +176,7 @@ const Options: NextPage = ({particles}: any) => {
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover, maximum-scale=6'/>
-        <title>Ludicrous - Settings</title>
+        <title>Lucid - Settings</title>
         <link rel='manifest' href='/manifest.json' />
         <link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
         <link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
